0f70b0437cc8d753569c30d08171d890b5bbd5d2
Ogban <ogbanugot@gmail.com>
2017-10-12 02:14:29 -0700

    Initial commit

---
+++ Desktop/numerical analysis code/bisection_method.py
@@ -0,0 +1,29 @@
+import sys
+import math
+'''bisection method
+        f is a function
+        a and  b are the end points'''
+def f(x):
+       return math.exp(x)-2-x
+       
+def bisection(a,b):
+       c = (a+b)/2.0
+       iteration = 0
+       while (b-a)/2.0:
+              iteration+=1
+              print("iteration number =",iteration)
+              if f(c) == 0:
+                      print("the zero is",c)
+                      break
+              elif f(a)*f(c) < 0:
+                     b = c
+              else :
+                     a = c
+              c = (a+b)/2.0
+              
+       return c
+       
+def main(x,y):
+        bisection(x,y)
+        
+print(main(0.5, 2.5))     
---
+++ Desktop/numerical analysis code/false_position.py
@@ -0,0 +1,26 @@
+import sys
+import math
+'''false position method
+        f is a function
+        a and  b are the end points'''
+maxItr = 100
+def f(x):
+       return math.exp(x)-2-x
+       
+def regFalsi(a,b):
+       iteration = 0
+       while iteration<maxItr:
+              x = a - f(a)*((b-a)/(f(b) - f(a)))
+              iteration+=1
+              print("iteration number =",iteration)
+              if f(x) == 0:
+                      print("the zero is",x)
+                      break
+              else:
+                  a = x
+                          
+       return x
+       
+def main(x,y):
+        regFalsi(x,y)
+        
